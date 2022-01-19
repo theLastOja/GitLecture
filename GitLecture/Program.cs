72d024f8043318b48bb7b460f25721da7ded0376
theLastOja <fiordi99@gmail.com>
2022-01-19 22:08:28 +0100

    Added method add

--- GitLecture/Program.cs
+++ GitLecture/Program.cs
@@ -6,7 +6,15 @@ namespace GitLecture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 10;
+            int num2 = 20;
+            int result = Add(num1, num2);
+            Console.WriteLine(result);
+        }
+
+        private static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
