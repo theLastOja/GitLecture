c0546ab485e58fe2db64ee4043131d190b0b913a
theLastOja <fiordi99@gmail.com>
2022-01-19 22:16:15 +0100

    Added method Divide

--- GitLecture/Program.cs
+++ GitLecture/Program.cs
@@ -10,11 +10,19 @@ namespace GitLecture
             int num2 = 20;
             int result = Add(num1, num2);
             Console.WriteLine(result);
+
+            result = Divide(num2, num1);
+            Console.WriteLine(result);
         }
 
         private static int Add(int num1, int num2)
         {
             return num1 + num2;
+        }
+
+        private static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
         }
     }
 }
