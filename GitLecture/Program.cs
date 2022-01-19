1deca1dcd33b4943680a1ec9d2eb2c2cf8f7120d
theLastOja <fiordi99@gmail.com>
2022-01-19 22:13:15 +0100

    Added method Multiply

--- GitLecture/Program.cs
+++ GitLecture/Program.cs
@@ -10,11 +10,19 @@ namespace GitLecture
             int num2 = 20;
             int result = Add(num1, num2);
             Console.WriteLine(result);
+
+            result = Multiply(num1, num2);
+            Console.WriteLine(result);
         }
 
         private static int Add(int num1, int num2)
         {
             return num1 + num2;
+        }
+
+        private static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
         }
     }
 }
